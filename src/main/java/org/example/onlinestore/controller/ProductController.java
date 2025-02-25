a4e10351f2bc8b390868c91571b48970bca58515
Olly <olly.aaa@mail.ru>
2025-02-26 00:17:33 +0500

    исправление ошибки сохранения категории

--- src/main/java/org/example/onlinestore/controller/ProductController.java
+++ src/main/java/org/example/onlinestore/controller/ProductController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -28,33 +26,21 @@ public class ProductController {
 
     @GetMapping("/catalog")
     public String filterProducts(
-            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String category, // Получаем категорию
             @RequestParam(required = false) BigDecimal minPrice,
             @RequestParam(required = false) BigDecimal maxPrice,
             @RequestParam(required = false) String sortPrice,
             @RequestParam(required = false) String sortRating,
             Model model) {
+
         List<Product> products = productService.filterProducts(category, minPrice, maxPrice, sortPrice, sortRating);
 
         model.addAttribute("categories", categoryService.getAllCategories());
         model.addAttribute("products", products);
-        model.addAttribute("category", category);
+        model.addAttribute("selectedCategory", category); // Передаём категорию в шаблон
         model.addAttribute("sortPrice", sortPrice);
         model.addAttribute("sortRating", sortRating);
 
         return "catalog";
-    }
-
-    @GetMapping("/product/{id}")
-    public String productDetails(@PathVariable Long id, Model model) {
-        Product product = productService.getProductById(id);
-        model.addAttribute("product", product);
-        return "product";
-    }
-
-    @GetMapping("/product/{productId}/average-rating")
-    @ResponseBody
-    public String getAverageRating(@PathVariable Long productId) {
-        return String.valueOf(productService.getAverageRating(productId));
     }
 }
